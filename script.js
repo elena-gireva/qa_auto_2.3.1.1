19b6a8806b7cf65bf9f7d8cc10ab0c8c25910aee
ElenaGireva <elena.webcode@gmail.com>
2023-12-18 18:18:43 +0100

    added second code HW2.3.1.2

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const players = [
-  { id: 1,
-    name: "Ivan",
-    scorePoints: 4500
-  },
-  { id: 2,
-    name: "Petr",
-    scorePoints: 23600
-  },
-  { id: 3,
-    name: "Vadim",
-    scorePoints: 3433
-  },
-  { id: 4,
-    name: "Olga",
-    scorePoints: 2356
-  }
+  { id: 1, name: "Александр", scorePoints: 100 },
+  { id: 2, name: "Екатерина", scorePoints: 150 },
+  { id: 3, name: "Михаил", scorePoints: 120 },
+  { id: 4, name: "Ольга", scorePoints: 80 },
+  { id: 5, name: "Владимир", scorePoints: 200 },
+  { id: 6, name: "Татьяна", scorePoints: 90 },
+  { id: 7, name: "Дмитрий", scorePoints: 110 },
+  { id: 8, name: "Наталья", scorePoints: 130 },
+  { id: 9, name: "Иван", scorePoints: 170 },
+  { id: 10, name: "Анна", scorePoints: 160 },
 ];
 
 // сразу присвоим счет первого объекта - будем с ним сравнивать все остальные перебором
